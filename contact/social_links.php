613f3239f5ac04e01c022ea449eb26115e02a623
jerlee123 <jerleeloo005@gmail.com>
2023-01-31 16:21:10 +0800

    Initial commit

---
+++ contact/social_links.php
@@ -0,0 +1,7 @@
+<div class="social-links">
+              <a href="https://twitter.com/JerleeLoo" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <!-- <a href="#" class="google-plus"><i class="bi bi-skype"></i></a> -->
+              <a href="https://www.linkedin.com/in/jr-loo-339600153/" class="linkedin"><i class="bi bi-linkedin"></i></a>
+</div>
